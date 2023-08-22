a68aeab3849f4edfb56a38d0e02b3c00240cf16e
Alexey Demin <mskdemin@gmail.com>
2023-08-22 20:28:22 +0300

    01

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
-import LoginAndRegistrationWithForm from '../LoginAndRegistrationWithForm/LoginAndRegistrationWithForm';
-import './Login.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
-export default function Login({ handelLoginSubmit, /* errorMessage */ }) {
-
+export default function Login({ handelLoginSubmit /* errorMessage */ }) {
   const [formValue, setFormValue] = useState({
-    email: '',
-    password: ''
+    email: "",
+    password: "",
   });
 
   const { email, password } = formValue;
@@ -17,7 +16,7 @@ export default function Login({ handelLoginSubmit, /* errorMessage */ }) {
       ...formValue,
       [name]: value,
     });
-  }
+  };
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -25,32 +24,52 @@ export default function Login({ handelLoginSubmit, /* errorMessage */ }) {
   }
 
   return (
-    <LoginAndRegistrationWithForm
-      titleText={'Вход'}
-      buttonText={'Войти'}
-      onSubmit={handleSubmit}
-    >
-      <input
-        className='Login__input'
-        placeholder='email@mail.com'
-        type='email'
-        name='email'
-        minLength='5'
-        required
-        onChange={handleChange}
-      />
-      <span id="input-name-error" className="error">{/* errorMessage */}</span>
-      <input
-        className='Login__input'
-        placeholder='••••••••••'
-        type='password'
-        name='password'
-        minLength='6'
-        maxLength='15'
-        required
-        onChange={handleChange}
-      />
-      <span id="input-name-error" className="error">{/* errorMessage */}</span>
-    </LoginAndRegistrationWithForm>
-  )
+    <form className="LoginAndRegistrationWithForm" onSubmit={handleSubmit}>
+      <div className="LoginAndRegistrationWithForm__container">
+        <h1 className="LoginAndRegistrationWithForm__title">{"Вход"}</h1>
+
+        <input
+          className="Login__input"
+          placeholder="email@mail.com"
+          type="email"
+          name="email"
+          minLength="5"
+          required
+          onChange={handleChange}
+        />
+        <span id="input-name-error" className="error">
+          {/* errorMessage */}
+        </span>
+        <input
+          className="Login__input"
+          placeholder="••••••••••"
+          type="password"
+          name="password"
+          minLength="6"
+          maxLength="15"
+          required
+          onChange={handleChange}
+        />
+        <span id="input-name-error" className="error">
+          {/* errorMessage */}
+        </span>
+
+        <button
+          type="submit"
+          name="button"
+          className="LoginAndRegistrationWithForm__button cursor"
+        >
+          {"Войти"}
+        </button>
+        {
+          <p className="login__register-text">
+            Уже зарегистрированы?{" "}
+            <Link className="login__register-link" to="/sign-up">
+              Войти
+            </Link>
+          </p>
+        }
+      </div>
+    </form>
+  );
 }

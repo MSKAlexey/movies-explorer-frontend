3562b231eebe0c08288e26954417cd97f5f957b7
Alexey Demin <mskdemin@gmail.com>
2023-09-05 18:01:06 +0300

    Register done

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 import "./Register.css";
 import logo from "../../images/logo.svg";
 
-export default function Register({ handelRegisterSubmit/* , errorMessage  */}) {
+export default function Register({
+  handelRegisterSubmit /* , errorMessage  */,
+}) {
   const [formValue, setFormValue] = useState({
     email: "",
     password: "",
@@ -25,73 +27,83 @@ export default function Register({ handelRegisterSubmit/* , errorMessage  */}) {
   }
 
   return (
-    <div className="Register__container">
-      <Link className="Register__logo-link" to="/">
-        <img className="Register__logo" src={logo} alt="Логотип" />
-      </Link>
-      <h1 className="Register__title">{"Добро пожаловать!"}</h1>
-
-      <form className="Register__form" onSubmit={handelSubmit}>
-        <div className="Register__form-input">
-          <span className="Register__title-input">Имя</span>
-          <input
-            className="Register__input"
-            placeholder="Ваше имя"
-            type="text"
-            name="name"
-            minLength="2"
-            maxLength="30"
-            required
-            onChange={handleChange}
-          />
-          <span id="input-name-error" className="Register__error">
-            {/* errorMessage */}
-          </span>
-        </div>
-        <div className="Register__form-input">
-          <span className="Register__title-input">E-mail</span>
-          <input
-            className="Register__input"
-            placeholder="pochta@yandex.ru|"
-            type="email"
-            name="email"
-            minLength="5"
-            maxLength="15"
-            required
-            onChange={handleChange}
-          />
-          <span id="input-name-error" className="Register__error">
-            {/* {errorMessage} */}
-          </span>
-        </div>
-        <div className="Register__form-input">
-          <span className="Register__title-input">Пароль</span>
-          <input
-            className="Register__input"
-            placeholder="••••••••••"
-            type="password"
-            name="password"
-            minLength="6"
-            maxLength="15"
-            required
-            onChange={handleChange}
-          />
-          <span id="input-name-error" className="Register__error">
-            {/* errorMessage */}
-          </span>
-        </div>
-      </form>
-
-      <button type="submit" name="button" className="Register__button cursor">
-        {"Зарегистрироваться"}
-      </button>
-
-      <p className="Register__text">
-        Уже зарегистрированы?{" "}
-        <Link className="Register__link" to="/sign-in">
-          Войти
+    <section className="Register">
+      <div className="Register__container">
+        <Link className="Register__logo-link" to="/">
+          <img className="Register__logo" src={logo} alt="Логотип" />
         </Link>
-      </p>
-    </div>
+        <h1 className="Register__title">{"Добро пожаловать!"}</h1>
+
+        <form className="Register__form" onSubmit={handelSubmit}>
+          <div className="Register__form-container">
+            <div className="Register__form-input">
+              <span className="Register__title-input">Имя</span>
+              <input
+                className="Register__input"
+                placeholder="Ваше имя"
+                type="text"
+                name="name"
+                minLength="2"
+                maxLength="30"
+                required
+                onChange={handleChange}
+              />
+              <span id="input-name-error" className="Register__error">
+                {/* errorMessage */}
+              </span>
+            </div>
+
+            <div className="Register__form-input">
+              <span className="Register__title-input">E-mail</span>
+              <input
+                className="Register__input"
+                placeholder="pochta@yandex.ru|"
+                type="email"
+                name="email"
+                minLength="5"
+                maxLength="15"
+                required
+                onChange={handleChange}
+              />
+              <span id="input-name-error" className="Register__error">
+                {/* {errorMessage} */}
+              </span>
+            </div>
+            <div className="Register__form-input">
+              <span className="Register__title-input">Пароль</span>
+              <input
+                className="Register__input"
+                placeholder="••••••••••"
+                type="password"
+                name="password"
+                minLength="6"
+                maxLength="15"
+                required
+                onChange={handleChange}
+              />
+              <span id="input-name-error" className="Register__error">
+                {/* errorMessage */}
+              </span>
+            </div>
+          </div>
+          <div className="Register__button-containet">
+            <button
+              type="submit"
+              name="button"
+              className="Register__button cursor"
+            >
+              {"Зарегистрироваться"}
+            </button>
+
+            <p className="Register__text">
+              Уже зарегистрированы?{" "}
+              <Link className="Register__link" to="/sign-in">
+                Войти
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 }

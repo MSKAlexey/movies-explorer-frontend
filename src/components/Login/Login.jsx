bf957150d5c06125c029f1ab42208be6015e616b
Alexey Demin <mskdemin@gmail.com>
2023-09-05 14:08:04 +0300

    Login done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -25,56 +25,66 @@ export default function Login({ handelLoginSubmit /* errorMessage */ }) {
   }
 
   return (
-    <div className="Login__container">
-      <Link className="Login__logo-link" to="/">
-        <img className="Login__logo" src={logo} alt="Логотип" />
-      </Link>
-      <h1 className="Login__title">{"Рады видеть!"}</h1>
+    <section className="Login">
+      <div className="Login__container">
+        <Link className="Login__logo-link" to="/">
+          <img className="Login__logo" src={logo} alt="Логотип" />
+        </Link>
+        <h1 className="Login__title">{"Рады видеть!"}</h1>
 
-      <form className="Login__form " onSubmit={handleSubmit}>
-        <div className="Login__form-input">
-          <span className="Login__title-input">E-mail</span>
-          <input
-            className="Login__input"
-            placeholder="pochta@yandex.ru|"
-            type="email"
-            name="email"
-            minLength="5"
-            required
-            onChange={handleChange}
-          />
-          <span id="input-name-error" className="error">
-            {/* errorMessage */}
-          </span>
-        </div>
-        <div className="Login__form-input">
-          <span className="Login__title-input">Пароль</span>
-          <input
-            className="Login__input"
-            placeholder="••••••••••"
-            type="password"
-            name="password"
-            minLength="6"
-            maxLength="15"
-            required
-            onChange={handleChange}
-          />
-          <span id="input-name-error" className="error">
-            {/* errorMessage */}
-          </span>
-        </div>
-        <button type="submit" name="button" className="Login__button cursor">
-          {"Войти"}
-        </button>
-        {
-          <p className="Login__text">
-            Ещё не зарегистрированы?{" "}
-            <Link className="Login__link" to="/sign-up">
-              Регистрация
-            </Link>
-          </p>
-        }
-      </form>
-    </div>
+        <form className="Login__form " onSubmit={handleSubmit}>
+          <div className="Login__form-container">
+            <div className="Login__form-input">
+              <span className="Login__title-input">E-mail</span>
+              <input
+                className="Login__input"
+                placeholder="pochta@yandex.ru|"
+                type="email"
+                name="email"
+                minLength="5"
+                required
+                onChange={handleChange}
+              />
+              <span id="input-name-error" className="error">
+                {/* errorMessage */}
+              </span>
+            </div>
+            <div className="Login__form-input">
+              <span className="Login__title-input">Пароль</span>
+              <input
+                className="Login__input"
+                placeholder="••••••••••"
+                type="password"
+                name="password"
+                minLength="6"
+                maxLength="15"
+                required
+                onChange={handleChange}
+              />
+              <span id="input-name-error" className="error">
+                {/* errorMessage */}
+              </span>
+            </div>
+          </div>
+          <div className="Login__button-container">
+            <button
+              type="submit"
+              name="button"
+              className="Login__button cursor"
+            >
+              {"Войти"}
+            </button>
+            {
+              <p className="Login__text">
+                Ещё не зарегистрированы?{" "}
+                <Link className="Login__link" to="/sign-up">
+                  Регистрация
+                </Link>
+              </p>
+            }
+          </div>
+        </form>
+      </div>
+    </section>
   );
 }

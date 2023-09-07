fda1c4415055665d29c8923e2aba3e1cc2a67624
Alexey Demin <mskdemin@gmail.com>
2023-09-07 22:11:26 +0300

    21

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,6 +1,6 @@
 // export const BASE_URL = 'https://api.alexey.nomoredomains.xyz';
-export const BASE_URL = 'http://localhost:3000';
-// export const BASE_URL = "https://api.alexey.nomoreparties.co";
+// export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = "https://api.alexey.nomoreparties.co";
 
 function checkStatusResponse(res) {
   if (res.ok) {

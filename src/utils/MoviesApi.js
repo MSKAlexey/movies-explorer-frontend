5af45b8e542e60d34eac1eb8ea7f3f25d153a3b5
Alexey Demin <mskdemin@gmail.com>
2023-09-14 09:16:21 +0300

    Исправил запрос фильмов при поиске убрав Authoriza

--- src/utils/MoviesApi.js
+++ src/utils/MoviesApi.js
@@ -9,13 +9,11 @@ class MoviesApi {
   }
 
   getMovies() {
-    // const token = localStorage.getItem("jwt");
     return fetch(`${this._url}`, {
       method: "GET",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
-        // Authorization: `Bearer ${token}`,
       },
     }).then(this._checkStatusResponse);
   }
